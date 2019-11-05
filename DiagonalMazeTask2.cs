5f9510b862c92798e552abee30307365ace658e4
YazanNajm <35304785+YazanNajm@users.noreply.github.com>
2019-11-06 01:45:29 +0500

    Add files via upload

---
+++ DiagonalMazeTask2.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+namespace Mazes
+{
+	public static class DiagonalMazeTask
+	{
+        
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+            Maze maze;
+            maze = new Maze(File.ReadAllLines(Path.Combine("mazes", "diagonal3" + ".txt")));
+
+            for (var i = 0; !robot.Finished; i++)
+            {
+                if (maze.IsWall(new System.Drawing.Point(robot.X + 1, robot.Y)))
+                    robot.MoveTo(Direction.Down);
+                else
+                    robot.MoveTo(Direction.Right);
+                if (robot.Finished)
+                    break;
+            }
+        }
+	}
+}
