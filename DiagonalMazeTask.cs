8ba2ee89fe97c82f13d34f0bc976a9947e6a6251
YazanNajm <35304785+YazanNajm@users.noreply.github.com>
2019-11-06 01:03:29 +0500

    Add files via upload

---
+++ DiagonalMazeTask.cs
@@ -0,0 +1,39 @@
+﻿namespace Mazes
+{
+	public static class DiagonalMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+            if (width > height)
+            {
+                for (var i = 0; !robot.Finished; i++)
+                {
+                    if (i % 2 == 0) 
+                    {
+                        for (var j = 0; j < 3; j++)
+                            robot.MoveTo(Direction.Right);
+                    }
+                    else
+                        robot.MoveTo(Direction.Down);
+                    if (robot.Finished)
+                        break;
+                } 
+            }
+            if (height > width)
+            {
+                for (var i = 0; !robot.Finished; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        for (var j = 0; j < 2; j++)
+                            robot.MoveTo(Direction.Down);
+                    }
+                    else
+                        robot.MoveTo(Direction.Right);
+                    if (robot.Finished)
+                        break;
+                }
+            }
+        }
+	}
+}
